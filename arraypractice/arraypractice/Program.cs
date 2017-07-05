279318495d07429b2aeecc5892c6f55d14510359
Jay <isaacsdaddy42293@gmail.com>
2017-07-05 11:09:44 -0400

    in class practice problems

--- arraypractice/arraypractice/Program.cs
+++ arraypractice/arraypractice/Program.cs
@@ -10,6 +10,43 @@ namespace arraypractice
     {
         static void Main(string[] args)
         {
+            ////declaring array without initializing
+            //int[] myFirstArray;
+            ////declaring and initializing a second array
+            //string[] myStringArray = { "First", "Second", "Third", "Fourth" };
+            ////declare and initialize a char array with the letters of your last name
+            //char[] myLastName = { 'M', 'i', 'l', 'L', 'e', 'r'};
+            //// i want my console to print three elements of my array
+            //Console.WriteLine(myLastName[0]);
+            //Console.WriteLine(myLastName[3]);
+            //Console.WriteLine(myLastName[1]);
+           
+            ////create following arrays and print using the correct index numbers
+            ////array of top 10 vacation spots
+            ////print array of the first and last vacation spots
+            ////array of the ages of 3 childrenr you know
+            ////array of GPA's on a 4.0 scale of 8 students - you dont have to print
+            ////array of first letter of 4 of your classmates names who are sitting near you
+            //// print all four on same line
+
+            //string[] vacationSpots = { "Sydney", "Orlando", "Puerto Rico", "Cuba", "California", "Canada", "DC", "Italy", "Tokyo", "Mexico" };
+            //Console.WriteLine(vacationSpots[0]);
+            //Console.WriteLine(vacationSpots[9]); 
+            //int[] kidsAge = { 1, 5, 7, };
+            //Console.WriteLine(kidsAge[0]);
+            //Console.WriteLine(kidsAge[1]);
+            //Console.WriteLine(kidsAge[2]);
+            //double[] gpas = { 3.2, 3.0, 1.2, 1.2, 3.2, 4.0, 4.0, 2.1 };
+            //char[] letter = { 'J', 'B', 'F', 'M' };
+            //for (int i = 0; i < letter.Length; i++)
+            //    Console.Write(letter[i] + ", ");
+
+
+            
+        
+
+
+        
         }
     }
 }

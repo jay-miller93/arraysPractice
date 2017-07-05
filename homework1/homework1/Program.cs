e5e2dd10bcde9e10da63c1cb5d522f52995b9487
Jay <isaacsdaddy42293@gmail.com>
2017-07-05 10:05:32 -0400

    first commit

---
+++ homework1/homework1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //{//First problem
+            //    double firstNumber;
+            //    double secondNumber;
+            //    Console.WriteLine("Please input your first numer.");
+            //    firstNumber = double.Parse(Console.ReadLine());
+
+            //    Console.WriteLine("Please input  your second number.");
+            //    secondNumber = double.Parse(Console.ReadLine());
+
+            //    if (firstNumber == secondNumber)
+            //    {
+            //        Console.WriteLine("The number " + firstNumber + " and " + secondNumber + " are the same.");
+
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("The numbers " + firstNumber + " and " + secondNumber + " are not equal to each other");
+            //    }
+            //fizz buzz
+            
+
+            Console.WriteLine("Please enter a test number");
+            int test = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter a Fizz number between 1 and 9");
+            int Fizz = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter a Buzz number between 1 and 9");
+            int Buzz = int.Parse(Console.ReadLine());
+
+            if (test / Fizz == Buzz && test / Buzz == Fizz)
+            {
+                Console.WriteLine("FIZZBUZZ!");
+
+            }
+
+            if (test % Fizz == 0)
+            {
+                Console.WriteLine("FIZZ!");
+            }
+            if (test % Buzz == 0 )
+            {
+                Console.WriteLine("BUZZ!");
+            }
+            else
+            {
+                Console.WriteLine(" The number is " + test);
+
+            }
+
+
+        }
+    }
+}
---
+++ switchcase/switchcase/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switchcase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string loyaltyLevel = "red";
+            switch (loyaltyLevel)
+            {
+                case "yellow":
+                    Console.WriteLine("Discount : 40%");
+                    break;
+                case "red":
+                    Console.WriteLine("Discount : 90");
+                    break;
+                default:
+                    Console.WriteLine("error");
+                    break;
+
+            }
+        }
+    }
+}
